afc9c472f425775e8929e82d5c11bfccf76a1126
isiagi <isiagigeofrey0@gmail.com>
2023-03-03 12:48:51 +0300

    adds config values

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'oa68mr4one'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-7831a6x3zyej01n8.us.auth0.com',            // Auth0 domain
+  clientId: 'Uy30pYyfB9obNon34K6sRbNFXVFsqjDi',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
